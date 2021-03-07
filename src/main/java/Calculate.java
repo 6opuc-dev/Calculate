7e4f8d8cc4817053ab89c0a4f10f824f651e94e9
6opuc-dev <75522440+6opuc-dev@users.noreply.github.com>
2021-03-07 10:53:21 +0300

    update

--- src/main/java/Calculate.java
+++ src/main/java/Calculate.java
@@ -12,7 +12,7 @@ public class Calculate {
         System.out.println("Допустимы целые числа от 1 до 10. Арабские или римские. Допустимы операторы \"+\",\"-\",\"*\",\"/\".");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
-        Pattern pt = Pattern.compile("^[1-9] [+\\-*/][1-9]$|^10 [+\\-*/] 10$|^10 [+\\-*/] [1-9]$|^[1-9] [+\\-*/] 10$");
+        Pattern pt = Pattern.compile("^[1-9] [+\\-*/] [1-9]$|^10 [+\\-*/] 10$|^10 [+\\-*/] [1-9]$|^[1-9] [+\\-*/] 10$");
         Pattern romanPt = Pattern.compile("^[IV]{1,4} [+\\-*/] [IV]{1,4}$|^I?X? [+\\-*/] [IV]{1,4}$|^[IV]{1,4} [+\\-*/] I?X?$|^I?X? [+\\-*/] I?X?$");
         Matcher m = pt.matcher(str);
         Matcher romanM = romanPt.matcher(str.toUpperCase());
@@ -21,16 +21,10 @@ public class Calculate {
             String[] numbers = str.toUpperCase().split(" ");
             if (m.find()) {
                 NumbersAndOperator nums = new NumbersAndOperator();
-                nums.setNum1(Integer.parseInt(numbers[0]));
-                nums.setOperator(numbers[1]);
-                nums.setNum2(Integer.parseInt(numbers[2]));
-                System.out.println("Результат: " + nums.operation());
+                System.out.println("Результат: " + nums.operation(Integer.parseInt(numbers[0]),numbers[1], Integer.parseInt(numbers[2])));
             } else if (romanM.find()) {
                 NumbersAndOperator nums = new NumbersAndOperator();
-                nums.setNum1(conv2.convert(numbers[0]));
-                nums.setOperator(numbers[1]);
-                nums.setNum2(conv2.convert(numbers[2]));
-                System.out.println("Результат: " + conv2.convert(nums.operation()));
+                System.out.println("Результат: " + conv2.convert(nums.operation(conv2.convert(numbers[0]),numbers[1], conv2.convert(numbers[2]))));
             } else {
                 throw new Exception("Ввод неверен");
             }
--- src/main/java/NumbersAndOperator.java
+++ src/main/java/NumbersAndOperator.java
@@ -1,38 +1,12 @@
 public class NumbersAndOperator {
-    public void setNum1(int num1) {
-        this.num1 = num1;
-    }
 
-    int num1;
-
-    public void setNum2(int num2) {
-        this.num2 = num2;
-            }
-
-    int num2;
-
-    public void setOperator(String operator) {
-        this.operator = operator;
-    }
-
-    String operator;
-
-    public int operation () {
-        int result = 0;
-        switch (operator) {
-            case "+":
-                result = num1 + num2;
-                break;
-            case "-":
-                result = num1 - num2;
-                break;
-            case "*":
-                result = num1 * num2;
-                break;
-            case "/":
-                result = num1 / num2;
-                break;
-        }
-        return result;
+    public int operation (int num1, String operator, int num2) {
+        return switch (operator) {
+            case "+" -> num1 + num2;
+            case "-" -> num1 - num2;
+            case "*" -> num1 * num2;
+            case "/" -> num1 / num2;
+            default -> 0;
+        };
     }
 }
